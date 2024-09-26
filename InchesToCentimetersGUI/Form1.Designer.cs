9d738c16e3744be83127fb0e5e2d0cddbe11c00e
Christine Hoang <christinehoangt@gmail.com>
2024-09-25 21:49:39 -0500

    Add project files.

---
+++ InchesToCentimetersGUI/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace InchesToCentimetersGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Inches = new TextBox();
+            Incheslbl = new Label();
+            ConvertButton = new Button();
+            Result = new Label();
+            SuspendLayout();
+            // 
+            // Inches
+            // 
+            Inches.Location = new Point(116, 77);
+            Inches.Name = "Inches";
+            Inches.Size = new Size(100, 23);
+            Inches.TabIndex = 0;
+            // 
+            // Incheslbl
+            // 
+            Incheslbl.AutoSize = true;
+            Incheslbl.Location = new Point(116, 59);
+            Incheslbl.Name = "Incheslbl";
+            Incheslbl.Size = new Size(71, 15);
+            Incheslbl.TabIndex = 1;
+            Incheslbl.Text = "Enter Inches";
+            // 
+            // ConvertButton
+            // 
+            ConvertButton.Location = new Point(116, 113);
+            ConvertButton.Name = "ConvertButton";
+            ConvertButton.Size = new Size(150, 25);
+            ConvertButton.TabIndex = 2;
+            ConvertButton.Text = "Convert to Centimeters";
+            ConvertButton.UseVisualStyleBackColor = true;
+            // 
+            // Result
+            // 
+            Result.AutoSize = true;
+            Result.ForeColor = Color.Blue;
+            Result.Location = new Point(116, 151);
+            Result.Name = "Result";
+            Result.Size = new Size(0, 15);
+            Result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 261);
+            Controls.Add(Result);
+            Controls.Add(ConvertButton);
+            Controls.Add(Incheslbl);
+            Controls.Add(Inches);
+            Name = "Form1";
+            Text = "Christine's Inches to Centimeters";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox Inches;
+        private Label Incheslbl;
+        private Button ConvertButton;
+        private Label Result;
+    }
+}
---
+++ InchesToCentimetersGUI/Form1.cs
@@ -0,0 +1,33 @@
+namespace InchesToCentimetersGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+         
+            {
+                int inches;
+                double centimeters;
+
+                
+                inches = Convert.ToInt32(Inches.Text); 
+
+             
+                centimeters = inches * 2.54;
+
+              
+                Result.Text = $"{inches} inches is {centimeters} cm.";
+            }
+        }
+
+        private void Result_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
